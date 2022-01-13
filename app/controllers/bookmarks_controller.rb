a55242e83b9dd90dd7e55d41ad8244fd66745067
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-01-14 01:46:57 +0900

    watchlist

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,18 +8,18 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
     @bookmark.list = @list
+
     if @bookmark.save
-      redirect_to list_path(@list)
-    else
-      render :new
+      redirect_to @list
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
-    @bookmark.destroy
-    redirect_to bookmark_path(@bookmarks)
+    @bookmark.delete
+    redirect_to @bookmark.list
   end
 
   private
