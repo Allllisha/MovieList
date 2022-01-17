0d8cfc542d7e32be098d9b1889bc32d25674502d
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-01-18 03:00:27 +0900

    turn.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import 'bootstrap';
 //= require jquery_ujs
 //= require_tree .
 require('test.js')
+require('turn.js')
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
--- app/javascript/test.js
+++ app/javascript/test.js
@@ -10,3 +10,40 @@ $(document).ready(function() {
 });
 
 
+
+
+$(function(){
+    $('#flipbook').turn(
+        {
+                // 自動でページをめくったときの高さ
+                elevation: 50,
+              
+                // ページめくりのスピード(ms)
+                duration: 1000,
+ 
+                // ページをめくるときの影->有効
+                gradients: true,
+ 
+                // 自動中央揃え->無効
+                autoCenter: false,
+ 
+                // 右開きか左開きかの設定->右開き
+                direction: 'ltr',
+
+                width: 500,
+                height: 600,
+               
+                display:'single' 
+        }
+    );
+  
+    $('#prevpage').click(function(){
+        $('#katudon_turn').turn('previous');
+    });
+ 
+    $('#nextpage').click(function(){
+        $('#katudon_turn').turn('next');
+    });
+ 
+});
+
