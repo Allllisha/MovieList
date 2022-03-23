ea9a4e13352bf26cf25d6f96df91cb84b21b48e0
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-03-24 03:27:29 +0900

    remove react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -49,6 +49,4 @@ document.addEventListener('turbolinks:load', () => {
 
 
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+
